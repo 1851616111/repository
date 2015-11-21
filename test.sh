b4d08a96244685b0b465fae9aad88fb99547e5af
1851616111 <1851616111@qq.com>
2015-11-21 16:12:59 +0800

    add test shell

--- test.sh
+++ test.sh
@@ -1,7 +1,28 @@
-curl http://127.0.0.1:8088/permission/chai -H user:chaizs@asiainfo.com
-curl http://127.0.0.1:8088/permission/chai -H user:chaizs@asiainfo.com -X PUT -d '{"username":"panxy4@asiainfo.com","opt_permission":1 }'
-curl http://127.0.0.1:8088/permission/chai?username=panxy3@asiainfo.com -H user:chaizs@asiainfo.com -X DELETE
+ curl http://10.1.235.98:8888/repositories/app -H "Authorization: Basic Y2hhaXpzQGFzaWFpbmZvLmNvbTo4ZGRjZmYzYTgwZjQxODljYTFjOWQ0ZDkwMmMzYzkwOQ===="
+curl http://10.1.235.98:8888/permission/chai -H user:chaizs@asiainfo.com
 
-curl http://127.0.0.1:8088/permission/chai/zong -H user:chaizs@asiainfo.com
-curl http://127.0.0.1:8088/permission/chai/zong -H user:chaizs@asiainfo.com -X PUT -d '{"username":"panxy01@asiainfo.com"}'
-curl http://127.0.0.1:8088/permission/chai/zong?username=panxy01@asiainfo.com -H user:chaizs@asiainfo.com -X DELETE
+permission
+-------------------------------------------------------------------------------------------------------
+curl http://10.1.235.98:8888/permission/chai -H "Authorization: Token 5c69d07b6143bc4c443564203b3704fd"
+curl http://10.1.235.98:8888/permission/chai -H "Authorization: Token 5c69d07b6143bc4c443564203b3704fd" -X PUT -d '{"username":"panxy4@asiainfo.com","opt_permission":1 }'
+curl http://10.1.235.98:8888/permission/chai?username=panxy4@asiainfo.com -H "Authorization: Token 5c69d07b6143bc4c443564203b3704fd" -X DELETE
+
+curl http://10.1.235.98:8888/permission/chai/zong -H "Authorization: Token 5c69d07b6143bc4c443564203b3704fd"
+curl http://10.1.235.98:8888/permission/chai/zong -H "Authorization: Token 5c69d07b6143bc4c443564203b3704fd" -X PUT -d '{"username":"panxy01@asiainfo.com"}'
+curl http://10.1.235.98:8888/permission/chai/zong?username=panxy01@asiainfo.com -H "Authorization: Token 5c69d07b6143bc4c443564203b3704fd" -X DELETE
+-------------------------------------------------------------------------------------------------------
+
+
+repositories
+-------------------------------------------------------------------------------------------------------
+curl -X PUT http://dogfood.hub.dataos.io/repositories/chai/zong  -H "Authorization: Token 5c69d07b6143bc4c443564203b3704fd" -d '{"comment":"123"}'
+curl -X GET http://10.1.235.98:8888/repositories/chai/zong  -H "Authorization: Token 5c69d07b6143bc4c443564203b3704fd"
+
+
+curl -X PUT http://54.223.58.0:8888/repositories/chai/zong  -H "Authorization: Token 5c69d07b6143bc4c443564203b3704fd" -d '{"comment":"123"}'
+curl -X GET http://54.223.58.0:8888/repositories/chai/zong
+-------------------------------------------------------------------------------------------------------
+
+
+curl http://54.223.58.0:8888/repositories/app -H "Authorization: Basic Y2hhaXpzQGFzaWFpbmZvLmNvbTo4ZGRjZmYzYTgwZjQxODljYTFjOWQ0ZDkwMmMzYzkwOQ===="
+curl http://54.223.58.0:8888/permission/chai -H user:chaizs@asiainfo.com
