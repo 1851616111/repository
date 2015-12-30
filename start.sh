0af9af78d62b565875bf86a524ff2378146b4cfe
1851616111 <1851616111@qq.com>
2015-12-30 14:00:33 +0800

    Merge branch 'develop' of https://github.com/asiainfoLDP/datahub_repository into develop
    # Conflicts:
    #	daocloud.yml

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 app=datahub_repository
 
 #pkill -f server
-export API_SERVER=10.1.235.98
+export API_SERVER=54.223.58.0
 export API_PORT=8888
 
 export goservice_port=8088
 
-export MONGO_PORT_27017_TCP_ADDR=10.1.235.98
-export MONGO_PORT_27017_TCP_PORT=27017
+export MONGO_PORT_27017_TCP_ADDR=54.223.58.0
+export MONGO_PORT_27017_TCP_PORT=27019
 export MONGO_ENV_MYSQL_USER=
 export MONGO_ENV_MYSQL_PASSWORD=
 export MQ_KAFKA_ADDR=10.1.235.98
