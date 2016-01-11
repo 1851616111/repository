3169e82f46c5e0f39db04f90baa50308abca12c4
1851616111 <1851616111@qq.com>
2016-01-11 19:49:45 +0800

    添加和删除协作后对repo更新cooperator字段

--- permission-handler.go
+++ permission-handler.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
 )
 
 const (
@@ -43,9 +44,24 @@ func setRepPmsHandler(r *http.Request, rsp *Rsp, param martini.Params, db *DB, p
 
 	selector := bson.M{COL_PERMIT_REPNAME: p.Repository_name, COL_PERMIT_USER: r_p.User_name}
 
-	if _, err := db.DB(DB_NAME).C(C_REPOSITORY_PERMISSION).Upsert(selector, r_p); err != nil {
-		return rsp.Json(400, ErrDataBase(err))
+	execs := []Execute{
+		{
+			Collection: C_REPOSITORY_PERMISSION,
+			Selector:   selector,
+			Update:     r_p,
+			Type:       Exec_Type_Upsert,
+		},
 	}
+	if r_p.Opt_permission == PERMISSION_WRITE {
+		exec := Execute{
+			Collection: C_REPOSITORY,
+			Selector:   bson.M{COL_REPNAME: p.Repository_name, COL_CREATE_USER: r_p.User_name},
+			Update:     bson.M{CMD_ADDTOSET: bson.M{COL_REP_COOPERATOR: r_p.User_name}},
+			Type:       Exec_Type_Update,
+		}
+		execs = append(execs, exec)
+	}
+	go asynExec(execs...)
 
 	return rsp.Json(200, E(OK))
 
@@ -70,6 +86,9 @@ func getRepPmsHandler(r *http.Request, rsp *Rsp, param martini.Params, db *DB, p
 	Q := bson.M{COL_REPNAME: p.Repository_name}
 	if user := strings.TrimSpace(r.FormValue("username")); user != "" {
 		Q[COL_PERMIT_USER] = user
+	}
+	if cooperator := strings.TrimSpace(r.FormValue("cooperator")); cooperator != "" {
+		Q[COL_PERMIT_WRITE] = PERMISSION_WRITE
 	}
 
 	l, err := db.getPermits(C_REPOSITORY_PERMISSION, Q, []int{page_index, page_size})
@@ -138,9 +157,27 @@ func delRepPmsHandler(r *http.Request, rsp *Rsp, param martini.Params, db *DB, p
 		exec[COL_PERMIT_USER] = cmdCondiction
 	}
 
-	if err := db.delPermit(C_REPOSITORY_PERMISSION, exec); err != nil {
-		return rsp.Json(400, ErrDataBase(err))
+	result := Rep_Permission{}
+	execs := []Execute{}
+	iter := db.DB(DB_NAME).C(C_REPOSITORY_PERMISSION).Find(exec).Iter()
+	for iter.Next(&result) {
+		exec := Execute{
+			Collection: C_REPOSITORY,
+			Selector:   bson.M{COL_REPNAME: result.Repository_name},
+			Update:     bson.M{CMD_PULL: bson.M{COL_REP_COOPERATOR: result.User_name}},
+			Type:      Exec_Type_Update,
+		}
+		execs = append(execs, exec)
 	}
+
+	go asynExec(execs...)
+
+	go func(db *DB, e bson.M) {
+		defer db.Close()
+		time.Sleep(time.Second)
+		db.delPermit(C_REPOSITORY_PERMISSION, exec)
+	}(db.copy(), exec)
+
 	return rsp.Json(200, E(OK))
 }
 
